68de83cb05ef96e60ad109b5010b5479a0421ea5
Sellig6792 <sellig6792@gmail.com>
2024-04-11 17:17:41 +0200

    Add tests for geometric functions
    To run the unittests you need to install `cargo wasi`, install `wasmtime` and then do `cargo wasi test`

--- tests/web.rs
+++ tests/web.rs
@@ -5,9 +5,19 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
+extern crate cazan_wasm;
+use cazan_wasm::geometry::*;
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
     assert_eq!(1 + 1, 2);
+}
+
+
+#[wasm_bindgen_test]
+fn test_distance() {
+    let a = Point { x: 0, y: 0 };
+    let b = Point { x: 3, y: 4 };
+    assert_eq!(distance(a, b), 5);
 }
